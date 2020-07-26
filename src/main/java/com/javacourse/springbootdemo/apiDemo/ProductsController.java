f822cebf6fafa7059148da208f75e021f8f7593b
zeyderan <aykutbayraktar34@gmail.com>
2020-07-26 13:02:46 +0300

    restcontroller ve get mapping örneği yapıldı

---
+++ src/main/java/com/javacourse/springbootdemo/apiDemo/ProductsController.java
@@ -0,0 +1,15 @@
+package com.javacourse.springbootdemo.apiDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductsController {
+	
+	@GetMapping("/")
+	public String get() {
+		
+		return "Laptop";
+		
+	}
+}
